85e2c8ba0e498a531b8b9d0093d128d94a439e66
brad <gjrjs31@naver.com>
2022-11-20 09:58:15 +0900

    feat: DetailView, Model 추가

---
+++ SwiftUI-open-market/SwiftUI-open-market/Model/DetailProduct.swift
@@ -0,0 +1,51 @@
+//
+//  DetailProduct.swift
+//  SwiftUI-open-market
+//
+//  Created by brad on 2022/11/19.
+//
+
+import Foundation
+
+struct DetailProduct: Codable {
+    let id, vendorID: Int
+    let name, welcomeDescription: String
+    let thumbnail: String
+    let currency: String
+    let price, bargainPrice, discountedPrice, stock: Int
+    let createdAt, issuedAt: String
+    let images: [DetailImage]
+    let vendors: Vendors
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case vendorID = "vendor_id"
+        case name
+        case welcomeDescription = "description"
+        case thumbnail, currency, price
+        case bargainPrice = "bargain_price"
+        case discountedPrice = "discounted_price"
+        case stock
+        case createdAt = "created_at"
+        case issuedAt = "issued_at"
+        case images, vendors
+    }
+}
+
+struct DetailImage: Codable {
+    let id: Int
+    let url, thumbnailURL: String
+    let issuedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, url
+        case thumbnailURL = "thumbnail_url"
+        case issuedAt = "issued_at"
+    }
+}
+
+// MARK: - Vendors
+struct Vendors: Codable {
+    let id: Int
+    let name: String
+}
--- SwiftUI-open-market/SwiftUI-open-market/Model/ProductAPI.swift
+++ SwiftUI-open-market/SwiftUI-open-market/Model/ProductAPI.swift
@@ -27,4 +27,22 @@ class ProductAPI {
             }
         }
     }
+    
+    func getProductDetail(id: Int, completion: @escaping (Bool, DetailProduct?) -> Void) {
+        let URL = "https://openmarket.yagom-academy.kr/api/products/\(id)"
+        
+        AF.request(URL,
+                   method: .get,
+                   parameters: nil,
+                   encoding: URLEncoding.default,
+                   headers: ["Content-Type":"application/json"])
+        .responseDecodable(of: DetailProduct.self) { response in
+            switch response.result {
+            case .success(let response):
+                completion(true, response)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
